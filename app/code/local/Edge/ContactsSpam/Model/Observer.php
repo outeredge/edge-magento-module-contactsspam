9b056bae35085816e8856cc943696137a5022a20
Bruno Dias <bruno@outeredgeuk.com>
2016-01-06 13:55:21 +0000

    Changed input element to have the same format as the original magento contact form

--- app/code/local/Edge/ContactsSpam/Model/Observer.php
+++ app/code/local/Edge/ContactsSpam/Model/Observer.php
@@ -12,14 +12,41 @@ class Edge_ContactsSpam_Model_Observer
             $dom = new DOMDocument;
             $dom->loadHTML($transport->getHtml());
 
-            $child = $dom->createElement('input');
-            $child->setAttribute('type', 'text');
-            $child->setAttribute('name', 'first_name');
-            $child->setAttribute('value', '');
-            $child->setAttribute('class', 'contact-first-name');
+            $div = $dom->createElement('div');
+            $div->setAttribute('class', 'field contact-first-name');
 
-            $form = $dom->getElementsByTagName('form')->item(0);
-            $form->appendChild($child);
+            $label = $dom->createElement('label');
+            $label->setAttribute('for', 'first-name');
+            $label->setAttribute('class', 'required');
+
+            $em  = $dom->createElement('em');
+            $txt = $dom->createTextNode("*");
+            $text = $dom->createTextNode("First Name");
+
+            $divInput = $dom->createElement('div');
+            $divInput->setAttribute('class', 'input-box');
+
+            $input = $dom->createElement('input');
+            $input->setAttribute('name', 'first_name');
+            $input->setAttribute('id', 'first-name');
+            $input->setAttribute('title', 'First-Name');
+            $input->setAttribute('value', '');
+            $input->setAttribute('class', 'input-text required-entry validate-email');
+            $input->setAttribute('type', 'text');
+
+            $label->appendChild($em);
+            $em->appendChild($txt);
+            $label->appendChild($text);
+
+            $div->appendChild($label);
+            $div->appendChild($divInput);
+            $divInput->appendChild($input);
+
+            $xpath = new DOMXPath($dom);
+            $fields = $xpath->query("//*[@class='fields']");
+
+            $form = $fields->item(0);
+            $form->appendChild($div);
 
             $transport->setHtml($dom->saveHTML());
         }
